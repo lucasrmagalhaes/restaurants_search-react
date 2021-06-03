18724ff960866a3cf7ab14de471585a2876deacb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-02 22:56:30 -0300

    Slider

--- restaurants-search/src/pages/Home/index.jsx
+++ restaurants-search/src/pages/Home/index.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
+import Slider from 'react-slick';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
 
 import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+    };
 
     return (
         <Wrapper>
@@ -27,6 +38,23 @@ const Home = () => {
                         />
                     </TextField>
                     <CarouselTitle>Na sua área</CarouselTitle>
+                    <Slider {...settings}>
+                        <div>
+                            <img src={restaurante}/>
+                        </div>
+                        <div>
+                            <img src={restaurante}/>
+                        </div>
+                        <div>
+                            <img src={restaurante}/>
+                        </div>
+                        <div>
+                            <img src={restaurante}/>
+                        </div>
+                        <div>
+                            <img src={restaurante}/>
+                        </div>
+                    </Slider>
                 </Search>
             </Container>
             <Map />
