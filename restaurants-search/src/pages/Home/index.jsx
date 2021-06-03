a166902ebd99a47c3cebe72cfc931fae64f27cf1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-02 22:29:11 -0300

    Wrapper

--- restaurants-search/src/pages/Home/index.jsx
+++ restaurants-search/src/pages/Home/index.jsx
@@ -4,26 +4,32 @@ import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
 
-import { Container, Search, Logo } from './styles';
+import { Container, Search, Logo, Wrapper, Map } from './styles';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
-    
+
     return (
-        <Container>
-            <Search>
-                <Logo src={logo} alt="Logo do restaurante" />
-                <TextField
-                    label="Pesquisar Restaurantes"
-                    outlined
-                    trailingIcon={<MaterialIcon role="button" icon="search"/>}>
-                    <Input
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                    />
-                </TextField>
-            </Search>
-        </Container>
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="Logo do restaurante" />
+                    
+                    <TextField
+                        label="Pesquisar Restaurantes"
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}
+                    >
+                        
+                        <Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
+                    </TextField>
+                </Search>
+            </Container>
+            <Map />
+        </Wrapper>
     );
 };
 
--- restaurants-search/src/pages/Home/styles.js
+++ restaurants-search/src/pages/Home/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
 export const Container = styled.aside`
     background-color: ${(props) => props.theme.colors.background};
     width: 360px;
@@ -17,4 +22,9 @@ export const Search = styled.section`
 
 export const Logo = styled.img`
     margin-bottom: 15px;
+`;
+
+export const Map = styled.div`
+    background-color: red;
+    width: 500px;
 `;
