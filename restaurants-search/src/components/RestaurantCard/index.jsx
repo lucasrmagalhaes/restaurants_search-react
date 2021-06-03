aa6a04bfeae221951500a706265f895bd25015ad
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-03 00:02:36 -0300

    Restaurant Info

--- restaurants-search/src/components/RestaurantCard/index.jsx
+++ restaurants-search/src/components/RestaurantCard/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-import { Restaurant } from './styles';
+import { Restaurant, RestaurantInfo, Title, Address } from './styles';
 
-const RestaurantCard = () => <Restaurant />
+const RestaurantCard = () => (
+    <Restaurant>
+        <RestaurantInfo>
+            <Title>Nome do Restaurante</Title>
+            <p>Avaliação</p>
+            <Address>Rua Cambotás 545</Address>
+        </RestaurantInfo>
+    </Restaurant>
+);
 
 export default RestaurantCard;
--- restaurants-search/src/components/RestaurantCard/styles.js
+++ restaurants-search/src/components/RestaurantCard/styles.js
@@ -12,3 +12,25 @@ export const Restaurant = styled.div`
         border-left-color: ${(props) => props.theme.colors.primary};
     }
 `;
+
+export const RestaurantInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Title = styled.span`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: ${(props) => props.theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 29px;
+    margin-bottom: 10px;
+`;
+
+export const Address = styled.span`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: ${(props) => props.theme.colors.text};
+    font-size: 16px;
+    line-height: 19px;
+    margin-bottom: 10px;
+`;
