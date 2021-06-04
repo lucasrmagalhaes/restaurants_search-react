bb6de783f7b4466c5e1a88974c2d128e2d7d0490
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-03 22:54:58 -0300

    Fazendo Buscas

--- restaurants-search/src/components/Map/index.jsx
+++ restaurants-search/src/components/Map/index.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
 export const MapContainer = (props) => {
     const [map, setMap] = useState(null);
-    const { google } = props;
+    const { google, query } = props;
+
+    useEffect(() => {
+        if (query) {
+            searchByQuery(query);
+        }
+    }, [query]);
+
+    function searchByQuery(query) {
+        const service = new google.maps.places.PlacesService(map);
+
+        const request = {
+            location: map.center,
+            radius: '200',
+            type: ['restaurant'],
+            query,
+        };
+
+        service.textSearch(request, (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                console.log('restaurants>>>', results);
+            }
+        });
+    }
 
     function searchNearby(map, center) {
         const service = new google.maps.places.PlacesService(map);
