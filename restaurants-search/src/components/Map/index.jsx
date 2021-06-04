a5a2e7fda5a7228f996579bff1d211827083fd6f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-03 22:05:03 -0300

    Restaurantes Próximos

--- restaurants-search/src/components/Map/index.jsx
+++ restaurants-search/src/components/Map/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
 export const MapContainer = (props) => {
+    const [map, setMap] = useState(null);
     const { google } = props;
 
-    return <Map google={google} centerAroundCurrentLocation />;
+    function searchNearby(map, center) {
+        const service = new google.maps.places.PlacesService(map);
+
+        const request = {
+            location: center,
+            radius: '20000',
+            type: ['restaurant'],
+        };
+
+        service.nearbySearch(request, (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                console.log('restaurants>>>', results);
+            }
+        });
+    }
+
+    function onMapReady(_, map) {
+        setMap(map);
+        searchNearby(map, map.center);
+    }
+
+    return <Map google={google} centerAroundCurrentLocation onReady={onMapReady} onRecenter={onMapReady} />;
 };
 
 export default GoogleApiWrapper({
