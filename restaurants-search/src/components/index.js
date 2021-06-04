e75c9e14f96ed9448211d1a273330d57ead551a9
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-04 13:26:21 -0300

    Skeleton Restaurant

--- restaurants-search/src/components/index.js
+++ restaurants-search/src/components/index.js
@@ -3,3 +3,4 @@ export { default as RestaurantCard } from './RestaurantCard';
 export { default as Modal } from './Modal';
 export { default as Map } from './Map';
 export { default as Loader } from './Loader';
+export { default as Skeleton } from './Skeleton';
--- restaurants-search/src/pages/Home/index.jsx
+++ restaurants-search/src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
 import restaurante from '../../assets/restaurante-fake.png';
-import { Card, RestaurantCard, Modal, Map, Loader } from '../../components';
+import { Card, RestaurantCard, Modal, Map, Loader, Skeleton } from '../../components';
 
 import { Container, Carousel, Search, Logo, Wrapper, CarouselTitle, ModalTitle, ModalContent } from './styles';
 
@@ -80,10 +80,21 @@ const Home = () => {
             </Container>
             <Map query={query} placeId={placeId} />
             <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}>
-                <ModalTitle>{restaurantSelected?.name}</ModalTitle>
-                <ModalContent>{restaurantSelected?.formatted_phone_number}</ModalContent>
-                <ModalContent>{restaurantSelected?.formatted_address}</ModalContent>
-                <ModalContent>{restaurantSelected?.opening_hours?.open_now ? 'Aberto' : 'Fechado'}</ModalContent>
+                {restaurantSelected ? (
+                    <>
+                        <ModalTitle>{restaurantSelected?.name}</ModalTitle>
+                        <ModalContent>{restaurantSelected?.formatted_phone_number}</ModalContent>
+                        <ModalContent>{restaurantSelected?.formatted_address}</ModalContent>
+                        <ModalContent>{restaurantSelected?.opening_hours?.open_now ? 'Aberto' : 'Fechado'}</ModalContent>
+                    </>
+                ) : (
+                    <>
+                        <Skeleton width="10px" height="10px" />
+                        <Skeleton width="10px" height="10px" />
+                        <Skeleton width="10px" height="10px" />
+                        <Skeleton width="10px" height="10px" />
+                    </>
+                )}
             </Modal>
         </Wrapper>
     );
